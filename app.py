0eea371912d309a9b0345152e30c8c10c3b5d191
daffiscoding <52997387+daffiscoding@users.noreply.github.com>
2022-09-21 23:22:41 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
 import os
 import datetime
+from unicodedata import name
 from flask import Flask, render_template, request
 from pymongo import MongoClient
-from dotenv import load_dotenv
 
-load_dotenv()
 
-def create_app():
-    app = Flask(__name__)
-    client = MongoClient(os.environ.get("MONGODB_URI"))
-    app.db = client.microblog
 
-    @app.route("/", methods=["GET", "POST"])
-    def home():
-        if request.method == "POST":
-            entry_content = request.form.get("content")
-            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.entries.insert({"content": entry_content, "date": formatted_date})
-        
-        entries_with_date = [
-            (
-                entry["content"],
-                entry["date"],
-                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
-            )
-            for entry in app.db.entries.find({})
-        ]
-        return render_template("home.html", entries=entries_with_date)
+
+app = Flask(__name__)
+client = MongoClient("mongodb+srv://daffiscoding:stemstem@microblog-application.jpr5yh1.mongodb.net/test")
+app.db = client.microblog
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        app.db.entries.insert({"content": entry_content, "date": formatted_date})
     
-    return app
+    entries_with_date = [
+        (
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in app.db.entries.find({})
+    ]
+    return render_template("home.html", entries=entries_with_date)
+    
+
+if __name__ == "__main__":
+    app.run()
